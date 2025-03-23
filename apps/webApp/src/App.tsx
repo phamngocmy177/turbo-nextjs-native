26cee042728b7e47bdd7e9b0f5c4758bde746641
phamngocmy177 <phamngocmy177@gmail.com>
2025-03-23 21:39:22 +0700

    chore: clean unused code

--- apps/webApp/src/App.tsx
+++ apps/webApp/src/App.tsx
@@ -1,8 +1,4 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-// import * as UI from '@repo/ui';
 import { Button } from '@repo/ui'
 import { TamaguiProvider, createTamagui } from 'tamagui'
 import { defaultConfig } from '@tamagui/config/v4'
@@ -12,24 +8,7 @@ const tamaguiConfig = createTamagui(defaultConfig)
 function App() {
   return (
     <TamaguiProvider config={tamaguiConfig}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
         <Button title={'Click me'} onClick={() => alert('Button clicked!')} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </TamaguiProvider>
   )
 }
